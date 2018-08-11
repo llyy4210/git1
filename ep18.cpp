59891644bad24b6db0dff9cbb74a448708a22cad
llyy4210 <870073049@qq.com>
2018-08-11 11:35:56 +0800

    new

---
+++ ep18.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: ep18.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年07月29日 星期日 14时21分43秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+#define MAX_N 30
+
+int map[MAX_N + 5][MAX_N + 5];
+int keep[MAX_N + 5][MAX_N + 5] = {0};
+
+int solve(int i, int j){
+    if(i == MAX_N) return 0;
+    if(keep[i][j])return keep[i][j];
+    int lnum = solve(i + 1, j), rnum = solve(i + 1, j + 1);
+    keep[i][j] = (lnum > rnum ? lnum :rnum) + map[i][j];
+    return keep[i][j];
+}
+
+int main(){
+    for(int i = 0; i < 15; i++){
+        for(int j = 0; j <= i; j++){
+            scanf("%d",map[i] + j);
+        }
+    }
+    printf("%d\n", solve(0,0));
+
+    return 0;
+
+}
